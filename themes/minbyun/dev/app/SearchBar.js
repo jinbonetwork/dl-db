2fd8ca9e0f14f0003024a0fcbdb36d8a2f368812
hodoug <j.hodoug@gmail.com>
2016-11-24 16:51:13 +0900

    리액트: 검색창 자료종류 핵심코드 완성

--- themes/minbyun/dev/app/SearchBar.js
+++ themes/minbyun/dev/app/SearchBar.js
@@ -1,11 +1,12 @@
 import React, {Component, PropTypes} from 'react';
 import DoctypeSelect from './searchBar/DoctypeSelect';
+import {_fieldAttrs} from './docSchema';
 
 class SearchBar extends Component {
 	constructor(){
 		super();
 		this.state = {
-			doctype: [],
+			doctypes: [],
 			keyword: undefined,
 			from: '',
 			to: ''
@@ -14,19 +15,23 @@ class SearchBar extends Component {
 	doctypeOptions(){
 		let options = {};
 		this.props.docData.taxonomy.doctype.forEach((value) => {
-			data[value] = this.props.docData.terms[value];
+			options[value] = this.props.docData.terms[value];
 		});
 		return options;
 	}
 	handleChange(which, arg, event){
 		if(which == 'doctype'){
+			this.setState({doctypes: arg})
 		}
 	}
 	render(){
 		let className = (this.props.mode == 'content' ? 'serchbar searchbar--content' : 'searchbar');
 		let searchBar = (
 			<div>
-				<DoctypeSelect options={this.doctypeOptions()} handleChange={this.handleChange.bind(this, 'doctype')} />
+				<DoctypeSelect displayName={_fieldAttrs['doctype'].displayName}
+					values={this.state.doctypes} options={this.doctypeOptions()}
+					handleChange={this.handleChange.bind(this, 'doctype')}
+				/>
 			</div>
 		);
 		return(
--- themes/minbyun/dev/app/searchBar/DoctypeSelect.js
+++ themes/minbyun/dev/app/searchBar/DoctypeSelect.js
@@ -1,45 +1,67 @@
 import React, {Component, PropTypes} from 'react';
+import {_isCommon} from '../functions';
 
-class Item extends Component {
-	constructor(){
-		super();
-		this.state = {
-			checkd: false
-		}
+class Option extends Component {
+	handleClick(){
+		this.props.handleClick({value: this.props.value, checked: !this.props.checked});
 	}
 	render(){
 		return (
-			<li></li>
+			<li onClick={this.handleClick.bind(this)}>
+				{(this.props.checked ? 'v ' : '')}
+				{this.props.name}
+			</li>
 		);
 	}
 }
-Item.propTypes = {
-	value: PropTypes.number.isRequired,
-	name: PropTypes.string.isRequired
-}
-
+Option.propTypes = {
+	value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+	name: PropTypes.string.isRequired,
+	checked: PropTypes.bool.isRequired,
+	handleClick: PropTypes.func.isRequired
+};
 
 class DoctypeSelect extends Component {
-	constructor(){
-		super();
-		this.state = {
-			selected: []
+	handleClick(which, arg, event){
+		if(which == 'option'){
+			let newValues;
+			if(arg.checked){
+				newValues = this.props.values;
+				newValues.push(arg.value)
+			} else {
+				newValues = [];
+				this.props.values.forEach((v) => {
+					if(v != arg.value) newValues.push(v);
+				});
+			}
+			this.props.handleChange(newValues);
 		}
 	}
 	render(){
 		let options = [];
 		for(let value in this.props.options){
-			options.push(<li key={value}>{this.props.options[value]}</li>);
+			options.push(
+				<Option key={value} value={value} name={this.props.options[value]}
+					checked={_isCommon([value], this.props.values)}
+					handleClick={this.handleClick.bind(this, 'option')}
+				/>
+			);
 		}
 
 		return (
 			<div className="doctype-select">
+				<div className="doctype-select__header">
+					<span>{this.props.displayName}</span>
+					<i className="pe-7s-angle-down"></i>
+				</div>
 				<ul>{options}</ul>
 			</div>
 		);
 	}
 }
 DoctypeSelect.propTypes = {
+	displayName: PropTypes.string.isRequired,
+	values: PropTypes.array.isRequired,
 	options: PropTypes.object.isRequired,
 	handleChange: PropTypes.func.isRequired
 };
