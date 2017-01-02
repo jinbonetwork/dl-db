7d3162a416e432dc4805f52f9c7fb5db7f7584cb
Kyuman Hwang <hwangkm@member.jinbo.net>
2017-01-02 22:28:19 +0900

    admin app 경로 지정

---
+++ app/admin/index.php
@@ -0,0 +1,11 @@
+<?php
+namespace DLDB\App\admin;
+
+$Acl = 'administrator';
+
+class index extends \DLDB\Controller {
+	public function process() {
+		$context = \DLDB\Model\Context::instance();
+	}
+}
+?>
