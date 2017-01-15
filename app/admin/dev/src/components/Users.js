c37876fb53c1403a39b9391af6a89eaa5456d8fd
hodoug <j.hodoug@gmail.com>
2017-01-15 22:46:39 +0900

    관리페이지 -- 회원목록: 검색, 추가, 삭제, 수정 버튼

--- app/admin/dev/src/components/Users.js
+++ app/admin/dev/src/components/Users.js
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from 'react';
+import {Link} from 'react-router';
 import CheckBox from '../accessories/CheckBox';
 import Pagination from '../accessories/Pagination';
 import {_mapO, _pushpull} from '../accessories/functions';
@@ -21,28 +22,64 @@ class Users extends Component {
 	}
 	render(){
 		const fProps = this.props.fieldData.fProps;
+		const listMenu = (
+			<tr className="users__menu">
+				<td colSpan="9">
+					<div>
+						<input type="text" className="users__keyword" />
+						<button className="users__search">검색</button>
+					</div>
+					<div>
+						<Link className="users__add-user" to="/admin/users/new">
+							<i className="pe-7s-add-user pe-va"></i><span>추가</span>
+						</Link>
+						<button className="users__delete-user">
+							<i className="pe-7s-delete-user pe-va"></i><span>삭제</span>
+						</button>
+					</div>
+				</td>
+			</tr>
+		);
 		const listHead = (
-			<tr>{_mapO(this.props.list[0], (pn, pv) => <th key={pn}>{fProps[pn].dispName}</th>)}</tr>
-		)
+			<tr className="users__head">
+				<td></td>
+				{_mapO(this.props.list[0], (pn, pv) => <td key={pn}>{fProps[pn].dispName}</td>)}
+				<td></td>
+				<td></td>
+			</tr>
+		);
 		const list = this.props.list.map((item, index) => {
 			let userInfo = _mapO(item, (pn, pv) => {
-				if(pn != 'id'){
-					return <td key={pn}>{pv}</td>
-				} else {
+				if(pn == 'id'){
 					return (
 						<td key={pn}>
 							<CheckBox check={this.props.selected.indexOf(pv) >= 0} onChange={this.handleChange.bind(this, 'check', pv)}/>
 						</td>
 					);
 				}
+				else if(pn == 'name'){
+					return <td key={pn}><Link to={'/admin/users/'+item.id}>{pv}</Link></td>
+				} else {
+					return <td key={pn}>{pv}</td>
+				}
 			});
-			return <tr key={index}>{userInfo}</tr>
+			return (
+				<tr key={index}>
+					<td></td>
+					{userInfo}
+					<td className="users__edit">
+						<Link to={'/admin/users/'+item.id+'/edit'}><i className="pe-7s-note pe-va"></i></Link>
+					</td>
+					<td></td>
+				</tr>
+			);
 		});
 		const page = (this.props.params.page ? parseInt(this.props.params.page) : 1);
 		return(
 			<div className="users">
 				<h1>회원목록</h1>
-				<table><tbody>
+				<table className="users__list"><tbody>
+					{listMenu}
 					{listHead}
 					{list}
 				</tbody></table>
