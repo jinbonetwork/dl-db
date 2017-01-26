f8b967ff3a935de7b5af61ea2efa68081056afb4
Kyuman Hwang <hwangkm@member.jinbo.net>
2017-01-26 17:41:41 +0900

    /api/admin/member/save 수정

--- app/api/admin/member/save.php
+++ app/api/admin/member/save.php
@@ -6,27 +6,54 @@ $Acl = 'administrator';
 class save extends \DLDB\Controller {
 	public function process() {
 		if($this->params['mode'] != 'add') {
-			if(!$this->params['id']) {
+			if(!$this->params['member']['id']) {
 				\DLDB\RespondJson::ResultPage( array( -2, '회원번호를 입력하세요') );
 			}
-			$member = \DLDB\Members::get($this->params['id']);
+			$member = \DLDB\Members::get($this->params['member']['id']);
 			if(!$member) {
 				\DLDB\RespondJson::ResultPage( array( -2, '존재하지 않는 회원입니다.') );
 			}
 			if($this->params['mode'] != 'delete') {
-				if(!$this->params['name']) {
+				if(!$this->params['member']['name']) {
 					\DLDB\RespondJson::ResultPage( array( -3, '이름을 입력하세요.') );
 				}
 			}
 		}
 		switch($this->params['mode']) {
 			case 'add':
+				$ret = \DLDB\Members\DBM::insert($this->params['member']);
+				if($ret < 0) {
+					\DLDB\RespondJson::ResultPage( array( -4, \DLDB\Members\DBM::getErrorMsg() ) );
+				}
+				$this->params['member']['id'] = $ret;
 				break;
 			case 'modify':
-				\DLDB\Members:modify($member,$this->params);
+				$ret = \DLDB\Members::modify($member,$this->params['member']);
+				if($ret < 0) {
+					\DLDB\RespondJson::ResultPage( array( -4, \DLDB\Members::getErrorMsg() ) );
+				}
 				break;
 			case 'delete':
+				$ret = \DLDB\Members\DBM::delete($member);
 				break;
+		}
+
+		if($this->params['mode'] != 'delete') {
+			$member = \DLDB\Members::get($this->params['member']['id']);
+			if($member['uid']) {
+				$member['role'] = \DLDB\Members\DBM::getRole($member['uid']);
+			} else {
+				$member['role'] = array();
+			}
+			$this->result = array(
+				'error' => 0,
+				'member' => $member
+			);
+		} else {
+			$this->result = array(
+				'error' => 0,
+				'message' => '삭제되었습니다.'
+			);
 		}
 	}
 }
