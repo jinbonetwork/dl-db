eb915042941baccdb42f0027b4b8cc779241b29f
Kyuman Hwang <hwangkm@member.jinbo.net>
2017-02-01 17:29:44 +0900

    관리자 이용약관 보기/수정 api

--- app/api/admin/agreement/index.php
+++ app/api/admin/agreement/index.php
@@ -8,6 +8,17 @@ class index extends \DLDB\Controller {
 		$this->params['output'] = 'json';
 
 		if($this->params['mode'] == 'modify') {
+			\DLDB\Options\saveOption('agreement',$this->params['content']);
+			$this->result = array(
+				'error' => 0,
+				'message' => '저장되었습니다.'
+			);
+		} else {
+			$agreement = \DLDB\Options::getOption('agreement');
+			$this->result = array(
+				'error' => ($agreement ? 0 : -1),
+				'agreement' => $agreement
+			);
 		}
 	}
 }
