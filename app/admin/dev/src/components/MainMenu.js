17d3afe186f9e98a38ee9cbbb52496882f715a76
hodoug <j.hodoug@gmail.com>
2017-02-22 17:04:04 +0900

    /admin: 게시판 관리 링크 추가

--- app/admin/dev/src/components/MainMenu.js
+++ app/admin/dev/src/components/MainMenu.js
@@ -1,5 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
+import {_wrap} from '../accessories/functions';
 
 class MainMenu extends Component {
 	handleClick(which){
@@ -8,18 +9,27 @@ class MainMenu extends Component {
 		}
 	}
 	render(){
+		const boardAdminUrl = _wrap(() => {
+			switch(this.props.loginType){
+				case 'xe': return '/xe?module=admin';
+				case 'gnu': return '/gnu5/admin';
+				case 'wp': return '/wordpress/wp-admin';
+			}
+		});
 		return (
 			<div className="main-menu">
 				<Link to="/admin/userlist"><span>회원 관리</span></Link>
 				<Link to="/admin/attachments"><span>첨부파일 관리</span></Link>
 				<Link to="/admin/agreement"><span>이용약관 수정</span></Link>
+				<a href={boardAdminUrl}><span>게시판 관리</span></a>
 				<a onClick={this.handleClick.bind(this, 'logout')}><span>로그아웃</span></a>
 			</div>
 		);
 	}
 }
 MainMenu.propTypes = {
-	onLogOut: PropTypes.func.isRequired
+	onLogOut: PropTypes.func.isRequired,
+	loginType: PropTypes.string.isRequired
 };
 
 export default MainMenu;
