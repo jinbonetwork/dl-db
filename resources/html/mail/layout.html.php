df5e93a5c0c4007ba4f509ad3cef8965abeb4431
Kyuman Hwang <hwangkm@member.jinbo.net>
2017-03-03 14:01:14 +0900

    기본 메일 layout 설정

---
+++ resources/html/mail/layout.html.php
@@ -0,0 +1,8 @@
+<h3 style="margin: 0; padding: 0.4em; border-bottom: 2px solid #989cff; background: #fff;">
+	<a href="<?php print $base_url; ?>" target="_blank" style="text-decoration: none;"><span style="color: #989cff; font-weight: bold; padding-left: 8px; font-size: 15px; letter-spacing: -.025em;"><?php print $site_title; ?></span></a>
+</h3>
+<div style="background: #f2f2f2; margin:0 auto; padding: 20px;">
+	<div style="padding: 20px;">
+		<?php print $content; ?>
+	</div>
+</div>
